a753dd46f40e94ffa3e7f2cd3734237ae3bfbb76
pmartinss-dev <pedro@pedromartins.org>
2024-08-21 17:31:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ window.onload = function() {
     const btn = document.getElementById('contactBtn');
     const span = document.getElementsByClassName('close')[0];
     const email = document.getElementById('email');
+    const notification = document.getElementById('notification');
 
     btn.onclick = function() {
         modal.style.display = 'block';
@@ -27,6 +28,11 @@ window.onload = function() {
 
     email.onclick = function() {
         navigator.clipboard.writeText(email.innerText)
-            .then(() => alert('Email copied to clipboard!'));
+            .then(() => {
+                notification.classList.add('show');
+                setTimeout(() => {
+                    notification.classList.remove('show');
+                }, 2000); // Hide notification after 2 seconds
+            });
     }
 };
