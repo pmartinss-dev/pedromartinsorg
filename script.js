8616820808f1eeca9bb4f3dc7949f0044a9e4678
pmartinss-dev <pedro@pedromartins.org>
2024-08-27 09:03:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ document.addEventListener('mousemove', function(e) {
     flashlight.style.left = `${e.clientX}px`;  // Use clientX for relative to viewport
     flashlight.style.top = `${e.clientY}px`;   // Use clientY for relative to viewport
 });
+
+function copyToClipboard(text) {
+    const tempTextarea = document.createElement("textarea");
+    tempTextarea.value = text;
+    document.body.appendChild(tempTextarea);
+
+    tempTextarea.select();
+    tempTextarea.setSelectionRange(0, 99999); 
+    document.execCommand("copy");
+
+    document.body.removeChild(tempTextarea);
+
+
+}
