5aeb6f4ad539701eab2a4fac4b2ffc23c472cc69
PM <pedro@pedromartins.org>
2024-08-27 12:37:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-document.addEventListener('mousemove', function(e) {
-    var flashlight = document.querySelector('.flashlight');
-    flashlight.style.left = `${e.clientX}px`; 
-    flashlight.style.top = `${e.clientY}px`;   
-});
+function isMobileDevice() {
+    return /Mobi|Android/i.test(navigator.userAgent) || window.matchMedia("(pointer: coarse)").matches;
+}
 
+if (!isMobileDevice()) {
+    document.addEventListener('mousemove', function(e) {
+        var flashlight = document.querySelector('.flashlight');
+        flashlight.style.left = `${e.clientX}px`;  // Use clientX for relative to viewport
+        flashlight.style.top = `${e.clientY}px`;   // Use clientY for relative to viewport
+    });
+}
 function copyToClipboard(text) {
     const tempTextarea = document.createElement("textarea");
     tempTextarea.value = text;
